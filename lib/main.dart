ac5bd0bff015486576cb49a6ec4923080ff2dd4a
Christopher Steven <christopherstevennunoo@gmail.com>
2024-05-28 13:40:36 +0000

    understanding widgets

--- lib/main.dart
+++ lib/main.dart
@@ -2,16 +2,17 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());  
+  runApp(const MyApp());  
 }
 
 class MyApp extends StatelessWidget{
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
 
-    return(Text(
-      "hello, world!!!!",
+    return(const Text(
+      "hello, world",
      textDirection: TextDirection.ltr,)
      );
   }
