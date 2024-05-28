4ee41d62add2ac9bed84908ef5a153b4ddab9456
Christopher Steven <christopherstevennunoo@gmail.com>
2024-05-28 15:19:22 +0000

    code review

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-
 import 'package:flutter/material.dart';
 
 void main() {
@@ -9,7 +8,7 @@ class MyApp extends StatelessWidget{
   const MyApp({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context){
 
     return(const Text(
       "hello, joy",
