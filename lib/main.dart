d26883cfa86c09d965de189f0a114be27f3beeee
Christopher Steven <christopherstevennunoo@gmail.com>
2024-06-03 12:34:56 +0000

    calling a class from a different file

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:currency_converter/currency_converter_material_page.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -11,11 +12,7 @@ class MyApp extends StatelessWidget{
   Widget build(BuildContext context){
 
     return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text("testing again"),
-        ),
-      ),
+      home: CurrencyConverterMarerialPage()
     );
   }
 }
